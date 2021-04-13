f185893ca105dc8fced0bb082dc40c31ffe86023
Tebogo Gerald Nkwane <nkwane.gerald@gmail.com>
2021-04-13 18:25:25 +0200

    Adding Security level Data to Database

--- TestingDatabaseMethodsCode/TestingDatabaseMethodsCode/Program.cs
+++ TestingDatabaseMethodsCode/TestingDatabaseMethodsCode/Program.cs
@@ -11,8 +11,12 @@ namespace TestingDatabaseMethodsCode
         static void Main(string[] args)
         {
             ContractMaintenanceAccess access = new ContractMaintenanceAccess();
-            //1 Security levels:
-            access.InsertSecuriyLevel("Standard", 1, "testing 12", "resting2");
+            //1 Security levels: Standard
+            access.InsertSecuriyLevel("Standard", 1, "MON-FRI-8AM-5PM", "MON-FRI-8AM-5PM");
+            // 2 Security levels: Premium
+            access.InsertSecuriyLevel("Premium", 1, "MON-FRI-5AM-12AM", "MON-FRI-5AM-12AM");
+            // 3 Security levels: Ultra
+            access.InsertSecuriyLevel("Ultra", 1, "MON-SUN-24HRS", "MON-SUN-24HRS");
             Console.ReadLine();
         }
     }
